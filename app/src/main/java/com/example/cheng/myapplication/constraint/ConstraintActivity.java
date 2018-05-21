f28a85f59ec2bddcc1b1b834a8b5ba4746719c32
xyz0z0 <xyz0z0.xyz@gmail.com>
2018-05-21 17:08:35 +0800

    nothing

---
+++ app/src/main/java/com/example/cheng/myapplication/constraint/ConstraintActivity.java
@@ -0,0 +1,15 @@
+package com.example.cheng.myapplication.constraint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cheng.myapplication.R;
+
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+    }
+}
